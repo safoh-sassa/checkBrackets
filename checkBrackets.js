65041d0ec9eb1daae8a890175d5b42e225df1ad6
safoh-sassa <safoh.sassa@gmail.com>
2023-01-17 00:01:46 +0100

    update to arrow function

--- checkBrackets.js
+++ checkBrackets.js
@@ -1,45 +1,45 @@
-function checkBrackets(arr) {
-    let tmp = [];
-  
-    if (arr.length == 0)
-      return false;
-  
-    if (!Number.isInteger(arr.length / 2))
-      return false
-  
-    for (let i = 0; i < arr.length; i++) {
-      let char = arr[i];
-  
-      if (char == '{' || char == '[' || char == '(') {
-        tmp.push(char);
-      }
-  
-      if (tmp.length == 0)
-        return false;
-  
-      switch (char) {
-        case '}':
-          lastChar = tmp.pop();
-          if (lastChar == '(' || lastChar == '[')
-            return false;
-          break;
-  
-        case ')':
-          lastChar = tmp.pop();
-          if (lastChar == '{' || lastChar == '[')
-            return false;
-          break;
-  
-        case ']':
-          lastChar = tmp.pop();
-          if (lastChar == '(' || lastChar == '{')
-            return false;
-          break;
-      }
+ const checkBrackets = (arr) => {
+  let tmp = [];
+
+  if (arr.length == 0)
+    return false;
+
+  if (!Number.isInteger(arr.length / 2))
+    return false
+
+  for (let i = 0; i < arr.length; i++) {
+    let char = arr[i];
+
+    if (char == '{' || char == '[' || char == '(') {
+      tmp.push(char);
     }
-    return true
+
+    if (tmp.length == 0)
+      return false;
+
+    switch (char) {
+      case '}':
+        lastChar = tmp.pop();
+        if (lastChar == '(' || lastChar == '[')
+          return false;
+        break;
+
+      case ')':
+        lastChar = tmp.pop();
+        if (lastChar == '{' || lastChar == '[')
+          return false;
+        break;
+
+      case ']':
+        lastChar = tmp.pop();
+        if (lastChar == '(' || lastChar == '{')
+          return false;
+        break;
+    }
   }
-  
-  console.log(checkBrackets("{()[]}"))
-  console.log(checkBrackets("{]"))
-  console.log(checkBrackets("([{}])"))
+  return true
+}
+
+console.log(checkBrackets("{()[]}"))
+console.log(checkBrackets("{]"))
+console.log(checkBrackets("([{}])"))
